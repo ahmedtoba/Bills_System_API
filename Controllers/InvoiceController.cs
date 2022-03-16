7c43aa75ed761af1313362cba89bca78c4f08348
Abdalla Tantawy <tant_9@yahoo.com>
2022-03-16 07:57:38 +0200

    Add Invoice

---
+++ Controllers/InvoiceController.cs
@@ -0,0 +1,30 @@
+﻿using Bills_System_API.Models;
+using Bills_System_API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Bills_System_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InvoiceController : ControllerBase
+    {
+        private readonly IInvoiceRepository invoiceRepository;
+
+        public InvoiceController(IInvoiceRepository invoiceRepository)
+        {
+            this.invoiceRepository = invoiceRepository;
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<Invoice>> GetInvoice()
+        {
+            return invoiceRepository.GetAll();
+        }
+        [HttpGet("{itemId}")]
+        public ActionResult<IEnumerable<Invoice>> GetByItem(int itemId)
+        {
+
+            return invoiceRepository.GetByItemId(itemId);
+           }
+    }
+}
